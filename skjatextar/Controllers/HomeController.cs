e29cc0f55e723576ba9b238717967a356443c267
petur215 <psigurdarson@gmail.com>
2014-05-06 15:12:27 +0000

    first version of project

---
+++ skjatextar/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace skjatextar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Þið eruð allir fávitar";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "smuuu";
+
+            return View();
+        }
+    }
+}
---
+++ skjatextar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="skjatextar.MvcApplication" Language="C#" %>
