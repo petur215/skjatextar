136cdc89f89db6cdfdbdabea37b2d59758659d1b
petur215 <psigurdarson@gmail.com>
2014-05-07 16:39:31 +0000

    eitthvað context eftir fyrirlestri hjá Patta

--- skjatextar/DAL/TranslationContext.cs
+++ skjatextar/DAL/TranslationContext.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using skjatextar.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -8,5 +10,17 @@ namespace skjatextar.DAL
 {
     public class TranslationContext : DbContext
     {
+        public TranslationContext()
+            : base("TranslationContext")
+        {
+
+        }
+
+        public DbSet<Translation> Translations { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
