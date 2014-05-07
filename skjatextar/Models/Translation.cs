b8345686670f6ae84e3c90e01e2497d54dca4335
petur215 <psigurdarson@gmail.com>
2014-05-07 16:00:30 +0000

    búinn til tranlsation model klasi og context

---
+++ skjatextar/Models/Translation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace skjatextar.Models
+{
+    public class Translation
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string text { get; set; }
+        public DateTime DateLastEdited { get; set; }
+        public string Category { get; set; }
+    }
+}
