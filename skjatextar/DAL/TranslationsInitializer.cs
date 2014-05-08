53f0d608a3dc8dfbb53840b19441294bfccc8863
petur215 <psigurdarson@gmail.com>
2014-05-08 18:33:07 +0000

    bug fix

--- skjatextar/DAL/TranslationsInitializer.cs
+++ skjatextar/DAL/TranslationsInitializer.cs
@@ -13,67 +13,67 @@ namespace skjatextar.DAL
             var translations = new List<Translation>
             {
                 new Translation{
-                    FileName= "Batman",
+                    Title= "Batman",
                     Text= "texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    FileName= "Batman 2",
+                    Title= "Batman 2",
                     Text= "texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    FileName= "Spiderman",
+                    Title= "Spiderman",
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    FileName= "This is it",
+                    Title= "This is it",
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    FileName= "Breaking bad S01E04",
+                    Title= "Breaking bad S01E04",
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    FileName= "Breaking bad S01E05",
+                    Title= "Breaking bad S01E05",
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    FileName= "Harry Potter 2",
+                    Title= "Harry Potter 2",
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    FileName= "Harry Potter 3",
+                    Title= "Harry Potter 3",
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    FileName= "Fargo",
+                    Title= "Fargo",
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    FileName= "Jango",
+                    Title= "Jango",
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    FileName= "Friends S01E07",
+                    Title= "Friends S01E07",
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    FileName= "Pulp Fiction",
+                    Title= "Pulp Fiction",
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    FileName= "Running with Scissors",
+                    Title= "Running with Scissors",
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
--- skjatextar/Models/Translation.cs
+++ skjatextar/Models/Translation.cs
@@ -8,7 +8,7 @@ namespace skjatextar.Models
     public class Translation
     {
         public int ID { get; set; }
-        public string FileName { get; set; }
+        public string Title { get; set; }
         public string Text { get; set; }
         public DateTime DateLastEdited { get; set; }
     }
