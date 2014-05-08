2740b64c676acafecba0e6da8f071047e4bee43c
Petur Bergmann <pjedur@gmail.com>
2014-05-08 05:42:56 +0000

    Allir model klasar komnir

---
+++ skjatextar/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace skjatextar.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        String Username { get; set; }
+        String Password { get; set; }
+    }
+}
