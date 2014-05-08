e7ac00518ab75eb3e23c0acb844bffed5d5424e4
antonorn <antonornth@gmail.com>
2014-05-08 13:21:59 +0000

    Tengdi Database fra Daniel

---
+++ skjatextar/Models/TranslationDataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace skjatextar.Models
+{
+    public class TranslationDataContext : DbContext
+    {
+        // ein lina fyrir hvern modelklasa
+        public DbSet<User> Users { get; set; }
+        // public DbSet<Request> Requests { get; set; } og svo framvegis.....
+    }
+}
