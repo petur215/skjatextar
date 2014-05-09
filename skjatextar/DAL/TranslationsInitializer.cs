dfd085a0a2520f2479a2ff2bbd50d90267b2ff9a
Vignir <vignirk@gmail.com>
2014-05-09 16:28:26 +0000

    baetti vid ID til ad vensla

--- skjatextar/DAL/TranslationsInitializer.cs
+++ skjatextar/DAL/TranslationsInitializer.cs
@@ -13,78 +13,98 @@ namespace skjatextar.DAL
             var translations = new List<Translation>
             {
                 new Translation{
+                    ID= 1,
                     Title= "Batman",
                     LikeCount= 3,
                     Text= "texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    Title= "Batman 2",
+                    ID= 2,
+                    Title= "Superman",
                     LikeCount= 5,
                     Text= "texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    Title= "Spiderman",
+                    ID= 2,
+                    Title= "Superman v2",
+                    LikeCount= 5,
+                    Text= "texti texti",
+                    DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
+                },
+                new Translation{
+                    ID= 4,
+                    Title= "Harry potter 2 v1",
                     LikeCount= 1,
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    Title= "This is it",
+                    ID= 4,
+                    Title= "Harry Potter 2 v3",
                     LikeCount= 41,
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
+                    ID= 5,
                     Title= "Breaking bad S01E04",
                     LikeCount= 10,
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
+                    ID= 6,
                     Title= "Breaking bad S01E05",
                     LikeCount= 12,
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    Title= "Harry Potter 2",
+                    ID= 3,
+                    Title= "Harry Potter 1 v2",
                     LikeCount= 11,
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
-                    Title= "Harry Potter 3",
+                    ID= 3,
+                    Title= "Harry Potter 1 v3",
                     LikeCount= 5,
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
+                    ID= 7,
                     Title= "Fargo",
                     LikeCount= 3,
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
+                    ID= 8,
                     Title= "Jango",
                     LikeCount= 44,
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
+                    ID= 9,
                     Title= "Friends S01E07",
                     LikeCount= 10,
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
+                    ID= 10,
                     Title= "Pulp Fiction",
                     LikeCount= 1,
                     Text= "texti texti texti",
                     DateLastEdited= DateTime.Parse("2014-05-07 16:58:00")
                 },
                 new Translation{
+                    ID= 11,
                     Title= "Running with Scissors",
                     LikeCount= 5,
                     Text= "texti texti texti",
