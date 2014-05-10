f28df65fd0d9a370e81c4531eb610551566e43a8
antonorn <antonornth@gmail.com>
2014-05-10 15:42:14 +0000

    Bjo til RequestRepo

--- skjatextar/Models/RequestRepository.cs
+++ skjatextar/Models/RequestRepository.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using skjatextar.DAL;
 
 namespace skjatextar.Models
 {
     public class RequestRepository
     {
+        TranslationContext m_db = new TranslationContext();
 
+        public Request GetRequestById(int id)
+        {
+            var result = (from s in m_db.Requests
+                          where s.RequestID == id
+                          select s).SingleOrDefault();
+
+            return result;
+        }
+        public void AddRequest(Request s)
+        {
+            m_db.Requests.Add(s);
+            m_db.SaveChanges();
+        }
+        public void Save()
+        {
+            m_db.SaveChanges();
+        }
     }
 }
