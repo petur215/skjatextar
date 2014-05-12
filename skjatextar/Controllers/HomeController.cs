8fb03d87c0e5210b22d7f71d79dbc74b22867b53
Petur Bergmann <pjedur@gmail.com>
2014-05-12 13:59:03 +0000

    PeturBSearchGluggi

--- skjatextar/Controllers/HomeController.cs
+++ skjatextar/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Net;
 using System.IO;
+using skjatextar.Repos;
 
 namespace skjatextar.Controllers
 {
@@ -34,14 +35,17 @@ namespace skjatextar.Controllers
             {
                 int realid = id.Value;
                 TranslationRepository repo3 = new TranslationRepository();
+                CommentRepository commentRepo = new CommentRepository();
                 var model = repo3.GetTranslationById(realid);
+                //var model = commentRepo.GetComments(); ???
+
                 return View(model);
             }
             return View("Error");
         }
 
         [HttpPost]
-        public void ViewTranslation(int id, object sender, EventArgs e)
+        public void ViewTranslation(int id, object sender, EventArgs e, FormCollection formData)
         {
             Translation s = repo.GetTranslationById(id);
             // Write the string to a file.
@@ -53,6 +57,35 @@ namespace skjatextar.Controllers
             Response.AddHeader("Content-Disposition", "attachment;filename=\"" + filePath + "\"");
             Response.TransmitFile(Server.MapPath(filePath));
             Response.End();*/
+
+            String strComment = formData["CommentText"];
+            if (!String.IsNullOrEmpty(strComment))
+            {
+                Comment c = new Comment();
+
+                c.CommentText = strComment;
+                String strUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                if (!String.IsNullOrEmpty(strUser))
+                {
+                    int slashPos = strUser.IndexOf("\\");
+                    if (slashPos != -1)
+                    {
+                        strUser = strUser.Substring(slashPos + 1);
+                    }
+                    //c.Username = strUser;
+
+                    CommentRepository commentRepo = new CommentRepository();
+                    commentRepo.AddComment(c);
+                }
+                else
+                {
+                    //c.Username = "Unknown user";
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("CommentText", "Comment text cannot be empty!");
+            }
         }
 
         public ActionResult Requests()
@@ -76,6 +109,12 @@ namespace skjatextar.Controllers
         public ViewResult Contact()
         {
             throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public ActionResult SearchWindow();
+        {
+            //herna kemur einhver snidugur kodi :D :)
         }
     }
 }
