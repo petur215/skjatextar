42b925435c9203fbe5ba712f2ec5afd0affd5443
Vignir <vignirk@gmail.com>
2014-05-13 18:19:29 +0000

    forsida

---
+++ skjatextar/Models/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace skjatextar.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Translation> Top10 { get; set; }
+        public IEnumerable<Translation> Newest10 { get; set; }
+    }
+}
