22fbfea695f2e3508f3704de3eb8cd5a59d0bff2
antonorn <antonornth@gmail.com>
2014-05-13 13:31:19 +0000

    Authorize a addRequest

--- skjatextar/Controllers/RequestController.cs
+++ skjatextar/Controllers/RequestController.cs
@@ -30,6 +30,7 @@ namespace skjatextar.Controllers
         }
 
         [HttpPost]
+        [Authorize]
         public ActionResult AddNewRequest(FormCollection formData)
         {
             //RequestViewModel r = new RequestViewModel();
